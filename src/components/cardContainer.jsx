d62d80af492780c1452dcd531c7d91a7a863e388
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2025-07-26 13:10:35 +0100

    Fixed Icon Issue

--- src/components/cardContainer.jsx
+++ src/components/cardContainer.jsx
@@ -1,6 +1,7 @@
 import images from "./images";
 import Card from "./card";
-
+import shuffle from "./gameLogic.js";
+console.log(shuffle(images));
 export default function CardContainer() {
   return (
     <div id="center-wrapper">
